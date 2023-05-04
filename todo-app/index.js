004df1fe70776e5adb104d51844a085449129019
bhavesh02github <127826427+bhavesh02github@users.noreply.github.com>
2023-05-04 15:48:38 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,4 +1,5 @@
 const app = require("./app"); 
- app.listen(process.env.PORT || 3000, () => { 
-   console.log("Started express server "); 
- }); 
+const port = process.env.PORT || 3000:
+ app.listen(port , () => { 
+   console.log("Started express server " + Port); 
+ });
