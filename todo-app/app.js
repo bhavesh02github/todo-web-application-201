19c8ebebdbbbf7006e1e6efcdad9c0b683d3fe38
bhavesh02github <127826427+bhavesh02github@users.noreply.github.com>
2023-05-22 21:13:13 +0530

    lvl 10

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,19 +1,26 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 const express = require("express");
-// eslint-disable-next-line no-unused-vars
 var csrf = require("tiny-csrf");
 const app = express();
-const { Todo } = require("./models");
 const bodypaser = require("body-parser");
 var cookieParser = require("cookie-parser");
-app.use(bodypaser.json());
 const path = require("path");
+
+const passport = require('passport'); //9.4k (gzipped: 2.9k)
+const connectEnsureLogin = require('connect-ensure-login'); //811 (gzipped: 368)
+const session = require('express-session'); //21.3k (gzipped: 7.2k)
+const LocalStrategy = require('passport-local'); //1.5k (gzipped: 719)
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+app.use(bodypaser.json());
+
 app.use(express.urlencoded({ extented: false }));
 app.use(cookieParser("shh! secret string"));
 app.use(csrf("123456789iamasecret987654321look", ["POST", "PUT", "DELETE"]));
 
 
+const { Todo, User } = require("./models");
 
 module.exports = {
   "**/*.js": ["eslint --fix", "prettier --write"],
@@ -22,14 +29,68 @@ module.exports = {
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(session({
+  secret: "my-super-secret-key-213158451656811651",
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000 //24hrs
+  }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, (username, password, done) => {
+  User.findOne({ where: { email: username }})
+   .then(async(user) => {
+    const result = await bcrypt.compare(password, user.password)
+    if (result) {
+      return done(null, user);
+    }
+    else {
+      return done("Invalid Password");
+    }
+
+   }).catch((error) => {
+      return done(error);
+   })
+}));
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session", user.id)
+  done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then(user => {
+      done(null, user)
+    })
+    .catch(error => {
+      done(error, null)
+    })
+});
+
 app.get("/", async (request, response) => {
+  // console.log(request.user) 
+  response.render("index", {
+    csrfToken: request.csrfToken(),
+  });
+});
+
+
+app.get("/todos", connectEnsureLogin.ensureLoggedIn(), async (request, response) => {
+  // console.log(request.user)
   const allTodos = await Todo.getTodos();
   const overdueTodos = await Todo.getoverdueTodos();
   const dueTodayTodos = await Todo.getdueTodayTodos();
   const dueLaterTodos = await Todo.getdueLaterTodos();
   const CompletedTodos = await Todo.getCompleted();
+
   if (request.accepts("html")) {
-    response.render("index", {
+    response.render("todos", {
       allTodos,
       overdueTodos,
       dueTodayTodos,
@@ -39,10 +100,51 @@ app.get("/", async (request, response) => {
     });
   } else {
     response.json({
-      allTodos, overdueTodos, dueTodayTodos, dueLaterTodos, CompletedTodos
+      allTodos, 
+      verdueTodos, 
+      dueTodayTodos, 
+      dueLaterTodos, 
+      CompletedTodos
     });
   }
 });
+
+app.get("/signup", (request, response) => {
+  response.render("signup", { title: "Signup", csrfToken: request.csrfToken(), 
+  })
+})
+
+app.post("/users", async (request, response) => {
+  // Hash password using bcrypt
+  const hashedPwd = await bcrypt.hash(request.body.password, saltRounds)
+  console.log(hashedPwd)
+  //console.log("Firstname", request.body.firstName);
+  try { 
+    const user = await User.create({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: hashedPwd
+    });
+    request.login(user, (err) => {
+      if(err) {
+        console.log(err)
+      }
+      response.redirect("/todos");
+    })
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+app.get("/login", (request, response) => {
+  response.render("login", { title: "Login", csrfToken: request.csrfToken()});
+})
+
+app.post("/session", passport.authenticate('local', { failureRedirect: "/login" }),(request, response) => {
+  console.log(request.user);
+  response.redirect("/todos");
+})
 
 app.get("/todos", async (request, response) => {
   console.log("Todo items", response.body);
