96ff97feb443bf47b46338e23dec33dcdbe8e5bf
bhavesh02github <127826427+bhavesh02github@users.noreply.github.com>
2023-05-23 15:23:28 +0530

    Update 20230519164220-add-user-id-in-todos.js

--- todo-app/migrations/20230519164220-add-user-id-in-todos.js
+++ todo-app/migrations/20230519164220-add-user-id-in-todos.js
@@ -3,30 +3,26 @@
 /** @type {import('sequelize-cli').Migration} **/
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // Remove the following lines
-    // await queryInterface.addColumn('Todos', 'userId', {
-    //   type: Sequelize.DataTypes.INTEGER
-    // })
+    await queryInterface.addColumn('Todos', 'userId', {
+    type: Sequelize.DataTypes.INTEGER
+    })
 
-    // await queryInterface.addConstraint('Todos', {
-    //   fields: ['userId'],
-    //   type: 'foreign key',
-    //   references: {
-    //     table: 'Users',
-    //     field: 'id'
-    //   }
-    // })
+    await queryInterface.addConstraint('Todos', {
+      fields: ['userId'],
+      type: 'foreign key',
+      references: {
+      table: 'Users',
+      field: 'id'
+      }
+    })
 
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    
+    
+    await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     
   },
 
   async down(queryInterface, Sequelize) {
-    // Remove the following line
-    // await queryInterface.removeColumn('Todos', 'userId')
+    await queryInterface.removeColumn('Todos', 'userId')
   }
 };
